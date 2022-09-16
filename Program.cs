cd218485532ccacffb1ee2af6aca08e4f53ac863
RachitShrivastava <rachitshrivastava15@gmail.com>
2022-09-16 14:57:28 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace EvenNumber
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = 100;
+            for(int i =0; i <=n; i++)
+            {
+                if (i %2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
